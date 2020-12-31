8658982ee3653b2c6bf0e30f8fd7ade91b4a1af4
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-31 16:02:42 +0300

    wired up currentUser

--- resolvers.ts
+++ resolvers.ts
@@ -12,9 +12,16 @@ export interface Context {
 
 export const resolvers = {
   Query: {
-    async users(prt: any, args: any, { User }: Context) {
-      const users = await User.find();
-      return users;
+    async currentUser(prt: any, args: any, { User, req }: Context) {
+      if (!req.headers.authorization) {
+        return null;
+      }
+      const token = jwt.verify(
+        req.headers.authorization.split(" ")[1],
+        process.env.JWT_SECRET!
+      );
+      const user = await User.findById(token);
+      return user;
     }
   },
   Mutation: {
