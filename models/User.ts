3ef6c5ffacce5badfc36fc9db9aa08f26986b34b
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-31 12:50:56 +0300

    added user model

---
+++ models/User.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+export interface UserAttrs {
+  email: string;
+  password: string;
+  fullName: string;
+}
+
+export interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  fullName: string;
+}
+
+export interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    fullName: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+        return ret;
+      }
+    },
+    timestamps: true
+  }
+);
+
+UserSchema.statics.build = (attrs: UserAttrs): UserDoc => {
+  return new User(attrs);
+};
+
+export const User = mongoose.model<UserDoc, UserModel>("User", UserSchema);
