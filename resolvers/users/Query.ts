f1bdb193b41e25cb4b11de6af16b87cb2db4fe6a
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-05 21:34:42 +0300

    fixed decoding token

--- resolvers/users/Query.ts
+++ resolvers/users/Query.ts
@@ -3,15 +3,13 @@ import { Context } from "../resolvers";
 
 export const UserQueries = {
   async currentUser(prt: any, args: any, { User, req }: Context) {
-    // if (!req.headers.authorization) {
-    //   return null;
-    // }
-    // const token = jwt.verify(
-    //   req.headers.authorization.split(" ")[1],
-    //   process.env.JWT_SECRET!
-    // );
-    console.log(req.headers);
-    const user = await User.findOne();
+    if (!req.session!.token) {
+      return null;
+    }
+    const token = jwt.verify(req.session!.token, process.env.JWT_SECRET!) as {
+      _id: string;
+    };
+    const user = await User.findById(token._id);
     return user;
   },
   users(prt: any, args: any, { User }: Context) {
