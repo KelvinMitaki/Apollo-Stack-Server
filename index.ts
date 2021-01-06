da0ac2caace8237dfaf6794af60da85caba20312
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-06 21:37:36 +0300

    added NODE_TLS_REJECT_UNAUTHORIZED

--- index.ts
+++ index.ts
@@ -7,10 +7,12 @@ import { typeDefs } from "./schema/typeDefs";
 import mongoose from "mongoose";
 import { User } from "./models/User";
 import { Agent } from "./models/Agent";
+console.log("reached");
 // @ts-ignore
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 const app = express();
 app.set("trust proxy", "1");
+console.log("reached 2");
 
 app.use(
   cookieSession({
@@ -22,6 +24,7 @@ app.use(
   })
 );
 
+console.log("reached 3");
 const server = new ApolloServer({
   typeDefs,
   resolvers,
