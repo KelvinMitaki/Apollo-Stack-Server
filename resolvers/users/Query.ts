7da0f27cf98d6c365cf43f8d084e346434ca266d
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-08 22:07:11 +0300

    fixed fetching current agent

--- resolvers/users/Query.ts
+++ resolvers/users/Query.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { Context } from "../resolvers";
 
 export const UserQueries = {
-  async currentUser(prt: any, args: any, { User, req }: Context) {
+  async currentUser(prt: any, args: any, { User, req, Agent }: Context) {
     if (
       !req.headers.cookie ||
       typeof req.headers.cookie === "undefined" ||
@@ -18,8 +18,11 @@ export const UserQueries = {
     ) as {
       _id: string;
     };
-
-    const user = await User.findById(token._id);
+    let user;
+    user = await User.findById(token._id);
+    if (!user) {
+      user = await Agent.findById(token._id);
+    }
     return user;
   },
   users(prt: any, args: any, { User }: Context) {
