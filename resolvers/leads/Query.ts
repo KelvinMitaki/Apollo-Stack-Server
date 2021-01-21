df030e44ac2df155115f599ee2ebb5265764879d
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-21 17:25:25 +0300

    added fetchLeads query

--- resolvers/leads/Query.ts
+++ resolvers/leads/Query.ts
@@ -3,8 +3,16 @@ import { AgentDoc } from "../../models/Agent";
 import { Context } from "../resolvers";
 
 export const LeadQueries = {
-  //     async fetchLeads(prt:any,args:{offset:number,limit:number},{req,Lead}:Context){
-  //         const agent:AgentDoc= isAuthorized(req,"agent")
-  // Lead.findOne({property:agent._id})
-  //     }
+  async fetchLeads(
+    prt: any,
+    args: { offset: number; limit: number },
+    { req, Lead }: Context
+  ) {
+    const agent: AgentDoc = await isAuthorized(req, "agent");
+    return Lead.find({ agent: agent._id }, null, {
+      skip: args.offset,
+      limit: args.limit,
+      populate: "Property"
+    });
+  }
 };
