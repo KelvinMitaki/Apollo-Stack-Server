c2e4b18fe59a8ec85b57c7fa1d6022327b7b37ad
KelvinMitaki <mitakikelvin1@gmail.com>
2021-01-25 12:03:58 +0300

    refactored isAuthorized middleware

--- middlewares/authorization.ts
+++ middlewares/authorization.ts
@@ -5,38 +5,20 @@ import { ForbiddenError } from "apollo-server-express";
 import { User, UserDoc } from "../models/User";
 
 export const isAuthorized = async (req: Request, usr: "user" | "agent") => {
-  if (
-    !req.headers.cookie ||
-    typeof req.headers.cookie === "undefined" ||
-    req.headers.cookie === "undefined" ||
-    req.headers.cookie.split("=").length < 2 ||
-    req.headers.cookie.split("=")[1].trim().length === 0
-  ) {
+  console.log(req.cookies);
+  if (!req.cookies || Object.keys(req.cookies).length === 0) {
     throw new ForbiddenError("unauthorized");
   }
-  const tokenArr = req.headers.cookie
-    .split("; ")
-    .map(t => ({ [t.split("=")[0]]: t.split("=")[1] }));
   let split_token;
-  split_token = tokenArr.find(
-    t =>
-      Object.values(t)[0].trim().length !== 0 &&
-      Object.keys(t)[0] === "client_token"
-  );
+  split_token = req.cookies["client_token"];
   if (!split_token) {
-    split_token = tokenArr.find(
-      t =>
-        Object.values(t)[0].trim().length !== 0 && Object.keys(t)[0] === "token"
-    );
+    split_token = req.cookies["token"];
   }
   if (!split_token) {
     throw new ForbiddenError("unauthorized");
   }
   try {
-    const token = jwt.verify(
-      Object.values(split_token)[0],
-      process.env.JWT_SECRET!
-    ) as {
+    const token = jwt.verify(split_token, process.env.JWT_SECRET!) as {
       _id: string;
     };
     let user;
